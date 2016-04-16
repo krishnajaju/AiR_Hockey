4c18289ddb1f7f422038aab8a20b3826b89b8eef
BlackPheonix <yvarcot@gmail.com>
2016-04-16 16:54:02 +0530

    Merge branch 'master' of https://github.com/BlackPheonix/AiR-Hockey
    # Conflicts:
    #	src/Client.py
    #	src/Constant.py
    #	src/Server.py
    #	src/disc.py
    #	src/image_detection.py
    #	src/mallet.py

--- src/Splash_screen.py
+++ src/Splash_screen.py
@@ -9,17 +9,18 @@ def client():
 
 
 pygame.init()
-screen = pygame.display.set_mode((100, 100))
+screen = pygame.display.set_mode((700, 500))
 s = pygame.image.load(SERVER_SS)
 c = pygame.image.load(CLIENT_SS)
-bg = pygame.image.load(BG_PATH)
-
+bg = pygame.image.load(SS)
+#font_main =
 while True:
     pygame.event.get()
-
+    screen.fill(black)
+    screen.blit(bg, (0, 0))
     pos = pygame.mouse.get_pos()
-
-    if pos[0] > 50:
-        print('RIGHT')
-    else:
-        print ('left')
+    if pos[0] > 350 and pos[1] > 0:
+        screen.blit(c, (338, 0))
+    elif pos[1] > 50:
+        screen.blit(s, (0, 0))
+    pygame.display.update()
